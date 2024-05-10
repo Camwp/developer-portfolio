a1b749b69d4d2ace24133cb50c1ac2a1464984cd
CWP <77127867+Camwp@users.noreply.github.com>
2024-05-10 11:24:26 -0700

    first commit

---
+++ utils/data/experience.js
@@ -0,0 +1,20 @@
+export const experiences = [
+  {
+    id: 1,
+    title: 'IT Specialist / Web Dev',
+    company: "PM Design Group Inc.",
+    duration: "(Jan 2018 - Present)"
+  },
+  {
+    id: 2,
+    title: "Software Tester  & Community Manager",
+    company: "Cask Protocol",
+    duration: "(Feb 2020 - Present)"
+  },
+  {
+    id: 3,
+    title: "Self Employed",
+    company: "CasualHorizons.",
+    duration: "(Feb 2020 - Present)"
+  }
+]
---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "Cameron Pedro",
+  profile: '/profile.png',
+  designation: "Software Developer",
+  description: "My name is Cameron Pedro. I am a professional and hobbyist programmer in my daily life. I am a passionate and detail-oriented programmer with a focus on creating intuitive user interfaces and robust backend systems. I am also adept at problem-solving and enjoy tackling new challenges in the world of software development. My core skillset based on JavaScript, Python, and C#. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'info@casualhorizons.com',
+  phone: '+19165792766',
+  address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
+  github: 'https://github.com/said7388',
+  facebook: 'https://www.facebook.com/abusaid.riyaz/',
+  linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
+  twitter: 'https://twitter.com/said7388',
+  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+  leetcode: "https://leetcode.com/said3812/",
+  devUsername: "Sundaii",
+  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+}
