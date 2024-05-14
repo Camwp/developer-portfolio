ad03d3a6cb231efebe07fe59db9495b3f01eb797
CWP <77127867+Camwp@users.noreply.github.com>
2024-05-13 22:35:20 -0700

    discvault

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -26,7 +26,7 @@ export const projectsData = [
     },
     {
         id: 3,
-        name: 'Disc Bag Builder',
+        name: 'Disc Vault',
         description: 'My team built an AI-based real estate app using Replicate API and OpenAI. We used Express, Typescript, OpenAI, Replicate, Stripe, and Mongoose to develop the API. We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.',
         tools: ['React', 'Bootstrap', 'SCSS', 'Stripe', 'Express', 'TypeScript', 'MongoDB', 'Azure Blob', 'OpenAI API', 'Replicate AI', 'Cronjob', 'JWT'],
         code: '',
